19d6370c70fda0df88ee0db1fc813d3b510711c5
maxvictos <106542699+maxvictos@users.noreply.github.com>
2023-02-15 15:42:18 -0300

    Initial commit

---
+++ projeto2/src/Titulo.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+function Titulo({ cor }) {
+
+    const [texto, setTexto] = useState('Título inicial');
+
+    const [inputText, setInputText] = useState('');
+
+    function clicou() {
+        setTexto(inputText);
+    }
+
+    return (
+        <div>
+            <h1 style={{ color: cor }}>{texto}</h1>
+            <input value={inputText} onChange={(e) => { setInputText(e.target.value)} } type='text' />
+            <button onClick={clicou}>Mudar</button>
+        </div>
+    )
+}
+
+export default Titulo
---
+++ projeto2/src/pages/Menu.jsx
@@ -0,0 +1,13 @@
+import App from '../App.css'
+
+function Menu() {
+    return (
+        <div className='menu'>
+            <a href='/' className='home'>Home</a>
+            <a href='/sobre' className='sobre'>Sobre</a>
+            <a href='/contato' className='contato'>Contato</a><br />
+        </div>
+    )
+}
+
+export default Menu
